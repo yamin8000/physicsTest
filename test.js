795aed6d323cf4d6ebcc35efe771d57780654457
Yamin Siahmargooei <yamin8000@yahoo.com>
2025-10-08 15:56:17 +0330

    Init

---
+++ test.js
@@ -0,0 +1,12 @@
+const button = document.querySelector("#button");
+const data = document.querySelector('#data');
+button.addEventListener('click', async (e) => {
+    data.replaceChildren();
+    const response = await fetch('https://jsonplaceholder.typicode.com/users/');
+    const json = await response.json();
+    json.forEach((user) => {
+        const userData = document.createElement("p");
+        userData.innerText = user.name;
+        data.appendChild(userData);
+    });
+})
